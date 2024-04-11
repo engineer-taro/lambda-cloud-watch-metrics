f7b51700c7966a87f9d0a02eea052b188c852db4
cm-engineer-taro <tsukamoto.taro@classmethod.jp>
2024-04-11 19:15:21 +0900

    パッケージのバージョンを更新し、voltaのnodeバージョンを18.12.0に設定 LambdaCloudWatchMetricsStackにAPI GatewayとLambda関数を追加

--- lib/lambda-cloud-watch-metrics-stack.ts
+++ lib/lambda-cloud-watch-metrics-stack.ts
@@ -1,16 +1,47 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as logs from "aws-cdk-lib/aws-logs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
 
 export class LambdaCloudWatchMetricsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const apiLogGroup = new logs.LogGroup(this, "api-access-log-group", {
+      logGroupName: "/aws/apigateway/api-access-log",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaCloudWatchMetricsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new apigateway.RestApi(this, "base-api", {
+      deploy: true,
+      deployOptions: {
+        tracingEnabled: true,
+        accessLogDestination: new apigateway.LogGroupLogDestination(
+          apiLogGroup
+        ),
+        accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields(),
+        metricsEnabled: true,
+        loggingLevel: apigateway.MethodLoggingLevel.INFO,
+        dataTraceEnabled: true,
+      },
+    });
+
+    const getUserLambda = new NodejsFunction(this, "get-user-lambda", {
+      entry: "src/get-user-handler.ts",
+      handler: "handler",
+      tracing: lambda.Tracing.ACTIVE,
+      bundling: {
+        externalModules: ["aws-sdk"],
+        forceDockerBundling: false,
+      },
+    });
+    const getUsersLambdaIntegration = new apigateway.LambdaIntegration(
+      getUserLambda,
+      {
+        proxy: true,
+      }
+    );
+    api.root.addResource("users").addMethod("GET", getUsersLambdaIntegration);
   }
 }
